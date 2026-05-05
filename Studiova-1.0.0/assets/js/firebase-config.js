98ad02ff349fe18304cefacc566e4f313542d864
Hai <Haitran1008@gmail.com>
2026-05-05 20:35:46 +0700

    first commit

---
+++ Studiova-1.0.0/assets/js/firebase-config.js
@@ -0,0 +1,22 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3TilSh068lP0b9AAspAYNNKk-M96Cr6I",
+  authDomain: "company-fd574.firebaseapp.com",
+  projectId: "company-fd574",
+  storageBucket: "company-fd574.firebasestorage.app",
+  messagingSenderId: "904377851384",
+  appId: "1:904377851384:web:11ce460258263702d18a0d",
+  measurementId: "G-FHM9EXEYXB"
+};
+export default firebaseConfig;
+// const app = initializeApp(firebaseConfig);
+
+// export const auth = getAuth(app);
+
+// const db = firebase.firestore();
+
+// const storage = firebase.storage();
